83d3a907c85a7dc2f501cddfa7663397868a0f51
bcapuano <bcapuano@asu.edu>
2022-12-28 13:16:46 -0500

    No testsuite

--- mayhem/fuzz_parse_operations.py
+++ mayhem/fuzz_parse_operations.py
@@ -1,24 +1,37 @@
 #! /usr/bin/env python3
+
 import atheris
 import sys
+import io
 import fuzz_helpers
+from contextlib import contextmanager
+with atheris.instrument_imports():
+    from hachoir.metadata.metadata import extractMetadata
 
-with atheris.instrument_imports(include=["hachoir"]):
-    import hachoir.parser
-    import hachoir.metadata
-
+import hachoir.parser
 # Exceptions
 from hachoir.stream.input import StreamError
+
 # Quiet the library's logging
 from hachoir.core import config
 config.quiet = True
 
+@contextmanager
+def nostdout():
+    save_stdout = sys.stdout
+    save_stderr = sys.stderr
+    sys.stdout = io.StringIO()
+    sys.stderr = io.StringIO()
+    yield
+    sys.stdout = save_stdout
+    sys.stderr = save_stderr
+
 def TestOneInput(data):
     fdp = fuzz_helpers.EnhancedFuzzedDataProvider(data)
     try:
-        with fdp.ConsumeMemoryFile(all_data=True) as fp, hachoir.parser.createParser(fp) as parser:
+        with fdp.ConsumeMemoryFile(all_data=True) as fp, nostdout(), hachoir.parser.createParser(fp) as parser:
             if parser:
-                hachoir.metadata.extractMetadata(parser)
+                extractMetadata(parser)
     except StreamError:
         return -1
     except AttributeError as e:
